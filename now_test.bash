ccf5dc7086937fc63ed636249992958f5171f989
Nozaki <sorakara656@gmail.com>
2023-12-29 21:59:54 +0900

    world

--- now_test.bash
+++ now_test.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SPDX-FileCopyrightText: 2023 Nozaki
 # SPDX-License-Identifier: BSD-3-Clause
-#set -x
+set -x
 ng () {
       echo Test at NG 
       res=1
@@ -15,12 +15,16 @@ output=$(./now)
 # 日時文字列がYYYY年MM月DD日 HH時MM分SS秒フォーマットに一致するかチェックする
 regex='^[0-9]{4}年[0-9]{2}月[0-9]{2}日 [0-9]{2}時[0-9]{2}分[0-9]{2}秒$'
 
-#echo "output: $output"
-#echo "regex: $regex"
+# 改行ごとに出力をチェック
+while IFS= read -r line; do
+    if ! [[ $line =~ $regex ]]; then
+        ng 
+        break
+    fi
+done <<< "$output"
 
-[[ "${output}" =~ "${regex}" ]] || ng $res
-
-[ "$res" = 0 ] && echo "テスト成功"
+# テスト結果に基づいて適切な処理を行う。
+[[ $res -eq 0 ]] && echo "テスト成功" || ng
 
 exit $res
 
