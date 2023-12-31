50d779cd81be2c6139629899b456988a570bb4ce
Nozaki <sorakara656@gmail.com>
2023-12-31 14:04:12 +0900

    cty

--- now_test.bash
+++ now_test.bash
@@ -12,13 +12,14 @@ res=0
 # nowから出力される日時文字列を取得
 output=$(./now | sed -n '2,$p')
 
-
-# 日時文字列がYYYY年MM月DD日 HH時MM分SS秒フォーマットに一致するかチェックする
+  
+# 日時文字列がYYYY年MM月DD日 HH時MM分SS秒フォーマットに一致する>かチェックする
 regex='^[0-9]{4}年[0-9]{2}月[0-9]{2}日 [0-9]{2}時[0-9]{2}分[0-9]{2}秒$'
-
 # 改行ごとに出力をチェック
 while IFS= read -r line; do
-    if ! [[ $line =~ $regex ]]; then
+    city_name=$(echo $line | cut -d ':' -f 1)
+    city_time=$(echo $line | cut -d ':' -f 2- | xargs)	
+    if ! [[ $city_time =~ $regex ]]; then
         ng
         break
     fi
